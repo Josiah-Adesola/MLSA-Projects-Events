2af8d4027c783a4142645164629ce50b1eddc088
Josiah-Adesola <josiahadesola2018@gmail.com>
2023-09-27 15:33:13 +0100

    created an app.py, allowed other audio file types

--- audio-to-text.py
+++ audio-to-text.py
@@ -4,12 +4,17 @@ import re
 import time
 from playsound import playsound
 import azure.cognitiveservices.speech as speechsdk
+from pydub import AudioSegment
 
 
 # Get Configuration Settings
 load_dotenv()
 cog_key = os.getenv('COG_SERVICE_KEY')
 cog_region = os.getenv('COG_SERVICE_REGION')
+
+def convert_audio_to_wav(input_file, output_file):
+    audio = AudioSegment.from_file(input_file)
+    audio.export(output_file, format="wav")
 
 def speech_recognize_continuous_from_file(file):
     audio_file_path = os.path.abspath(file)
@@ -21,8 +26,11 @@ def speech_recognize_continuous_from_file(file):
 def main():
     try:
         # Create the speech recognizer using the function
-        audio_file = "dream.wav"
-        speech_recognizer = speech_recognize_continuous_from_file(audio_file)
+        # Convert the input audio file to .wav
+        input_audio_file = "dream.m4a"  # Replace with the path to your input audio file
+        output_audio_file = "output_audio.wav"
+        convert_audio_to_wav(input_audio_file, output_audio_file)
+        speech_recognizer = speech_recognize_continuous_from_file(output_audio_file )
         #playsound(audio_file)
 
         # Define a callback function for recognized speech
