710434e4b1c5ff94f072b2eb040424e373567a0d
Josiah-Adesola <josiahadesola2018@gmail.com>
2023-10-17 11:14:18 +0100

    reverted the app.py

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
 import streamlit as st
 import os
 import tempfile
-import hashlib  # For generating unique file names
 from audio2text import transcribe_audio
 
 def main():
     st.title("Audio to Text Transcription")
 
-    uploaded_audio = st.file_uploader("Upload an audio file (MP3, WAV, m4a, etc.)", type=["mp3", "wav", "m4a"])
+    uploaded_audio = st.file_uploader("Upload an audio file (MP3, WAV,m4a, etc.)", type=["mp3", "wav", "m4a"])
 
     if uploaded_audio is not None:
-        # Generate a unique file name based on the file's content
-        file_hash = hashlib.md5(uploaded_audio.read()).hexdigest()
-        temp_audio_path = os.path.join(tempfile.gettempdir(), f"{file_hash}.wav")
-
-        # Save the uploaded audio to the unique temporary file
-        with open(temp_audio_path, "wb") as temp_audio_file:
-            temp_audio_file.write(uploaded_audio.read())
+        # Save the uploaded audio to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio:
+            temp_audio.write(uploaded_audio.read())
+            temp_audio_path = temp_audio.name
 
         st.audio(temp_audio_path, format="audio/wav")
 
         if st.button("Transcribe"):
-            with st.spinner("Transcribing..."):
+            with st.spinner("Transcribing.z.."):
                 result = transcribe_audio(temp_audio_path)
                 if result is not None:
                     st.write("Transcription Result:")
                     st.success(result)
                 else:
                     st.write("Transcription failed or returned 'None'.")
+
 
 if __name__ == "__main__":
     main()
