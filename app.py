8bc0e28457d5a23aac88fcd27caba13dd79e8ea2
Josiah-Adesola <josiahadesola2018@gmail.com>
2023-10-17 11:30:00 +0100

    unique names for the output-audio.wav file

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import streamlit as st
 import os
 import tempfile
+import time
+import uuid  # For generating a unique ID
 from audio2text import transcribe_audio
+
+def generate_unique_filename():
+    # Generate a unique filename based on the current timestamp and a random component (UUID)
+    timestamp = int(time.time())
+    unique_id = str(uuid.uuid4().hex)[:8]  # Extract the first 8 characters of the UUID
+    filename = f"output_audio_{timestamp}_{unique_id}.wav"
+    return filename
 
 def main():
     st.title("Audio to Text Transcription")
 
-    # Use a constant filename for the output_audio.wav
-    output_audio_filename = "output_audio.wav"
+    # Generate a unique filename for the output_audio.wav
+    output_audio_filename = generate_unique_filename()
     output_audio_path = os.path.join(tempfile.gettempdir(), output_audio_filename)
 
     uploaded_audio = st.file_uploader("Upload an audio file (MP3, WAV, m4a, etc.)", type=["mp3", "wav", "m4a"])
 
     if uploaded_audio is not None:
-        # Save the uploaded audio to the output_audio.wav file
+        # Save the uploaded audio to the generated unique filename
         with open(output_audio_path, "wb") as output_audio_file:
             output_audio_file.write(uploaded_audio.read())
 
