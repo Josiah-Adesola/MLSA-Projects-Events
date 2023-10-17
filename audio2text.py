86b8e411bbe1f638e5b93d9dc21b10717218fa38
Josiah-Adesola <josiahadesola2018@gmail.com>
2023-10-17 11:31:54 +0100

    reduced the speech_recognizer listening time

--- audio2text.py
+++ audio2text.py
@@ -51,7 +51,7 @@ def transcribe_audio(input_audio_file):
         speech_recognizer.start_continuous_recognition()
 
         # Wait for recognition to complete (adjust the duration as needed)
-        time.sleep(30)  # This allows recognition to continue for 10 seconds
+        time.sleep(12)  # This allows recognition to continue for 10 seconds
 
         # Stop continuous recognition
         speech_recognizer.stop_continuous_recognition()
